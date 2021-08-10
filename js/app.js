15d8f2864d2be53e759d5a417477a9fe06642eb0
Mateus pereira <you@example.com>
2021-08-10 11:51:51 -0300

    curso de javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,135 @@
-window.alert("Olá Mundo")
+// -Browser Object Model (BOM) Elementos do window
+console.log(
+    window.screen.width,
+    window.screen.height,
+    window.navigator,
+    //window.location.href = "https://www.linkedin.com/in/mateus-pereira-00b705133/"
+)
+
+function voltar(){
+    history.back()
+}
+
+function avancar(){
+    history.forward()
+}
+
+console.log(window) // mostra todos os atributos do windows no console
+
+
+
+// -Document Object Model (DOM)Elementos da página
+//alert("Olá Mundo")
+console.log(
+    document.getElementById("titulo").innerHTML
+)
+
+document.getElementById("largura").innerHTML = screen.width
+
+console.log(
+    document.getElementsByClassName("carro")[0]
+)
+
+console.log(
+    document.getElementsByTagName("div")[4].innerHTML
+)
+
+console.log(
+    document.querySelector(".div2") //se for class usa ponto e id usa #
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")
+)
+
+var paragrafo = document.getElementById("p1");
+paragrafo.innerHTML = "Novo paragrafo";
+paragrafo.style.color = "red";
+paragrafo.style.backgroundColor = "blue";
+paragrafo.style.height = "20px";
+
+var imagem = document.getElementById("imagem")
+imagem.src = "./imagens/javascript.jpg"
+imagem.alt = "Teste de imagem"
+imagem.width = "80"
+
+//Create Element
+var imagem = document.createElement('img')
+imagem.src = "./imagens/javascript.jpg"
+
+var paragrafo = document.createElement("p")
+paragrafo.innerHTML = "Olá pessoal"
+
+console.log(
+    imagem, paragrafo
+)
+
+//appendChild e RemoveChild
+var el = document.createElement("p")
+el.innerHTML = "Elemento filho!"
+
+var im = document.createElement("im")
+im.src = "./imagens/javascript.jpg"
+
+document.getElementById("idConteudo").appendChild(el),
+document.getElementById("idConteudo").appendChild(im)
+
+document.getElementById("idConteudo").removeChild(im)
+
+//childNodes
+// console.log(
+//     document.body.childNodes //retorna todos os filhos do body
+// )
+
+//nodeName, nodeType, nodeValue
+var lista = document.body.childNodes //retorna todos os filhos do body
+//alert(lista[12].nodeName) 
+    //nodeType retorna 1- nó de elemento, 2 - atributo, 3 - texto, 8 - comentário
+    //nodeValue retorna o valor do nó
+
+//parentNode
+console.log(
+    document.getElementById("brasil").parentNode
+)
+
+
+//setAttribute, getAtribute, removeAttribute
+document.getElementById("btn-set").addEventListener("click", function() {
+    document.getElementById("title").setAttribute("class", "red")
+})
+
+document.getElementById("btn-remove").addEventListener("click", function() {
+    document.getElementById("title").removeAttribute("class")
+})
+
+document.getElementById("btn-get").addEventListener("click", function() {
+    var value = document.getElementById("title").getAttribute("class")
+    document.getElementById("class").innerHTML = value
+})
+
+//createTextNode, textContent
+var title = document.querySelector("h2")
+var txt = document.createTextNode("Um texto qualquer")
+
+title.appendChild(txt)
+title.textContent = "Um novo texto qualquer"
+
+
+//Listas children e insertBefore
+var list = document.getElementsByTagName("ul")[0]
+var itens = list.children
+
+var novoItem = document.createElement("li")
+novoItem.textContent = "Suco de Uva"
+
+list.insertBefore(novoItem, itens[2])// ele vai colocar o suco de uva depois do indice 2
+
+var list2 = document.getElementsByTagName("ul")[1]
+var itens2 = list2.children
+
+var novoItem2 = document.createElement("li")
+novoItem2.textContent = "Margarina"
+
+list2.replaceChild(novoItem2, itens2[2])
+
+console.log(list2)
---
+++ js/app2.js
@@ -0,0 +1,200 @@
+//Tipos de dados
+
+//string
+var nomeCompleto = "mateus pereira"
+//number
+var idade = 25
+//boolean
+var solteiro = true
+//array
+var frutas = ["goiaba", "maça", "banana"]
+//object
+var carro = new Object()
+carro.fabricacao = "2025"
+carro.cor = "azul"
+//function
+var somar = function(a, b) {return a+b}
+
+//alert(typeof(soma(6,7)))
+
+
+//variáveis
+//escopo global, escopo local
+//var (escopo global), let (dentro do bloco), const
+
+//Const não consegue alterar a variável
+const meuNome = "Mateus Pereira"
+
+if(true) {
+    //se utilizar var ele escapa do escopo e pode ser lido no console.log
+    var nome = "Mateus"
+
+    //se utilizar let ele não sera reconhecido fora do escopo
+    let name = "Pereira"
+}
+
+console.log(nome)
+
+//Array
+//E uma estrutura de dados que armazena uma coleção de elementos
+let meuArray = [
+    "notebook", 
+    1500.00, 
+    {memoria: '16gb', hd: '1tb'},
+    ['notebook dell', 'modelo 1000']
+]
+console.log(meuArray[2].memoria)// será acessado o atributo do objeto
+console.log(meuArray[3][1])
+
+//Objeto com propriedades
+let pessoa = {
+    nome: "camila",
+    idade: 35,
+    feliz: true,
+    pets: ["tartaruga", "coelho"],
+    carro: { //Objeto dentro de um objeto
+        camaro: {
+            placa: "123456",
+            cor: "amarelo"
+        },
+        uno: {
+            placa: "kmx-1546",
+            cor: "branco"
+        }
+    },
+
+    andar: function(km){
+        //alert(pessoa.nome+" andou "+km+" km")
+    }
+}
+
+//pessoa.andar(20)
+console.log(pessoa.pets[0])
+console.log(pessoa.carro.camaro.cor)
+
+//Operadores Aritméticos
+let x = 10
+let y = 5
+let resultado = x*y //x%y //x**y
+
+let n1 = 8
+let n2 = 7
+let n3 = 6
+let media = (n1+n2+n3)/3
+
+console.log(x+y)
+console.log(resultado)
+console.log(media)
+
+//Operadores de Atribuição
+let m = 5
+let n = 3
+//m = m+n
+m += n //x **= n
+m++
+console.log(m)
+
+
+//Operadores Lógicos, Comparação e Ternário
+let o = 5
+let idadeEleitor = 16
+console.log(o === "5")//Comparação 3 iguais compara se o tipo tbm é igual
+// !== verifica o tipo e apresenta true
+// < <= > >= && ||
+let eleitor = (idadeEleitor >= 16) ? "pode votar" : "não pode votar"
+console.log(eleitor)// Ternário
+
+/* Condicionais: 
+If
+Else
+ElseIf
+Switch
+*/
+let pais = "Brasil"
+let estado = "RJ"
+
+if(pais == "Brasil") {
+    console.log("Sou Brasileiro")
+} else if (estado == "RJ") {
+    console.log("Sou carioca")
+}else {
+    console.log("Sou gringo")
+}
+
+switch(pais){
+    case 'Brasil':
+        console.log("Sim, Brasil")
+        break
+    case 'Canada': 
+        console.log("Opa, Canadá")
+        break
+    default:
+        console.log("Padrão")
+        break
+}
+
+/* Condicionais:
+for
+while
+*/
+let paises = document.getElementsByClassName('pais')
+let inc = 0
+
+for (let i = 0; i < paises.length; i++) {
+    console.log(paises[i].innerHTML)
+}
+
+// while(inc < paises.length) {//enquanto
+//     console.log(paises[inc].innerHTML)
+//     inc++
+// }
+do {
+        console.log(paises[inc].innerHTML)
+        inc++
+    }
+while (inc < paises.length) //enquanto
+
+/* Loops: 
+for in
+for of
+*/
+
+let fruta = {
+    nome: "Banana", 
+    preco: 5.99, 
+    unidade:1
+}
+
+let aparelhos = [
+    "Celular", 
+    "Fone de ouvido", 
+    "Microfone",
+    "Mouse"
+]
+
+for (let valor in fruta) {
+    console.log(fruta[valor]) //valor
+}
+
+for (let val of aparelhos) {
+    console.log(val)
+}
+
+//Function
+function soma (x, y) {
+    //alert("Funcionou!")
+    return x+y
+}
+console.log(soma(30, 5))
+document.getElementById("resultado").innerHTML = soma(30, 10)
+
+
+/* Alert = notifica, Prompt = recebe parametros e Confirm = true ou false */
+let caixaTexto = prompt("Digite seu nome")
+console.log(caixaTexto)
+alert(caixaTexto)
+let resposta = confirm("Tem certeza que deseja excluir esse item?")
+
+if(resposta == true) {
+    alert("Item excluido com sucesso!")
+} else { alert("Exclusão cancelada") }
